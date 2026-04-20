80866fb17ee7e5eed27ed4a92f6daf8cc67d6cb5
Santiago Joshua <santiago.dev.pe@gmail.com>
2026-04-20 11:32:57 -0500

    Fix: Add missing code to the path selection function

---
+++ Inventory_program_s1/practice_of_.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+
+from pathlib import Path
+
+def select_path() -> Path:
+    while True:
+        try:
+            user_inp = input("Write a direction to continue or press Enter to continue in the current path:\n").strip()
+            
+            if user_inp == "":
+                base_path = Path.cwd()
+                print(f"The folder that will be used to create subfolders is {base_path}.")
+                return base_path
+            
+            selected_path = Path(user_inp)
+
+            if selected_path.exists() and selected_path.is_dir():
+                print("The folder specified by the user will be used.")
+                return selected_path
+            else:
+                print("The path does not exist or is not a directory.")
+                
+        except OSError as e:
+            print(f"Error: Invalid system operation or illegal characters. Details: {e}")
+        
+def create_dir() -> Path:
+    pass
+      
+
+
+
+
